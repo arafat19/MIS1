0f2c1ed48f89f0344afda5b18642514093e56b2a
arafat19 <arafat0112358@gmail.com>
2015-10-22 17:31:42 +0600

    new commit

---
+++ plugins/applicationPlugin/test/unit/com/athena/mis/application/taglib/CostingTypeDropDownTagLibSpec.groovy
@@ -0,0 +1,20 @@
+package com.athena.mis.application.taglib
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(CostingTypeDropDownTagLib)
+class CostingTypeDropDownTagLibSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
